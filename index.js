463ddde07cd42fd3132ea6339abc48482394d079
gyujae <jsgy0000@gmail.com>
2019-08-26 05:43:32 +0000

    마크다운에서 HTML로 변환 적용

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const http = require('http');
+const fs = require('fs');
+const md = require('markdown-it')();
 const port = 3000;
-
-http.createServer( (request, response) => {
+function toHtml(markdown) {
+return `
+	<DOCTYPE html>
+	<html lang="ko">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	${md.render(markdown)}
+	</body>
+	</html>`;
+}
+http.createServer((request, response) => {
 	console.log('Request received.');
-	
-	fs.readFile('view/README.md', (error, data) =>{
+
+	fs.readFile('view/README.md', 'utf8', (error, data) => {
 		console.log('Read file - README.md');
-		response.writeHead(200, {'Content-Type' : 'text/html'});
-		response.write('Hello Git!');
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(toHtml(data));
 		response.end();
 	});
+	
 }).listen(port);
+
+console.log(`Server has stared - port:${port}`);
